74e8106012de54f2647c4ed841ea85a2334792a9
FrancisTicgal <francisco@ticgal.com>
2025-09-08 14:50:56 +0200

    phpstan, php-cs-fixer, workflow, twig checker, new scripts, phpdoc and refactor

--- ajax/footer.php
+++ ajax/footer.php
@@ -1,33 +1,35 @@
 <?php
-/*
- -------------------------------------------------------------------------
- Footer plugin for GLPI
- Copyright (C) 2024 by the TICgal Team.
- https://www.tic.gal
- -------------------------------------------------------------------------
- LICENSE
- This file is part of the Footer plugin.
- Footer plugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
- Footer plugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with Footer. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- @package   Footer
- @author    the TICgal team
- @copyright Copyright (c) 2024 TICgal team
- @license   AGPL License 3.0 or (at your option) any later version
-				http://www.gnu.org/licenses/agpl-3.0-standalone.html
- @link      https://www.tic.gal
- @since     2024
- ----------------------------------------------------------------------
-*/
 
+/**
+ * -------------------------------------------------------------------------
+ * Footer plugin for GLPI
+ * Copyright (C) 2025 by the TICGAL Team.
+ * https://www.tic.gal
+ * -------------------------------------------------------------------------
+ * LICENSE
+ * This file is part of the Footer plugin.
+ * Footer plugin is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * Footer plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Footer. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @package   footer
+ * @author    the TICGAL team
+ * @copyright Copyright (c) 2024-2025 TICGAL team
+ * @license   AGPL License 3.0 or (at your option) any later version
+ *            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link      https://www.tic.gal
+ * @since     2024
+ * -------------------------------------------------------------------------
+ */
+
+/** @var \DBmysql $DB */
 global $DB;
 
 include("../../../inc/includes.php");
@@ -41,68 +43,68 @@ use Glpi\Toolbox\Sanitizer;
 $config = new PluginFooterConfig();
 
 if (isset($_POST['action']) && $_POST['action'] == 'get_footer') {
-	$query = [
-		'FROM' => PluginFooterLink::getTable(),
-		'WHERE' => getEntitiesRestrictCriteria(PluginFooterLink::getTable(), '', '', true),
-	];
+    $query = [
+        'FROM' => PluginFooterLink::getTable(),
+        'WHERE' => getEntitiesRestrictCriteria(PluginFooterLink::getTable(), '', '', true),
+    ];
 
-	$link = [
-		'config' => [
-			'fixed' => $config->fields['fixed'],
-		],
-		'links' => []
-	];
-	foreach ($DB->request($query) as $data) {
-		$link['links'][] = [
-			'url' => DropdownTranslation::getTranslatedValue(
-				$data['id'],
-				PluginFooterLink::getType(),
-				'url',
-				$_SESSION['glpilanguage'],
-				$data['url']
-			),
-			'name' => Sanitizer::decodeHtmlSpecialChars(DropdownTranslation::getTranslatedValue(
-				$data['id'],
-				PluginFooterLink::getType(),
-				'name',
-				$_SESSION['glpilanguage'],
-				$data['name']
-			)),
-		];
-	}
+    $link = [
+        'config' => [
+            'fixed' => $config->fields['fixed'],
+        ],
+        'links' => [],
+    ];
+    foreach ($DB->request($query) as $data) {
+        $link['links'][] = [
+            'url' => DropdownTranslation::getTranslatedValue(
+                $data['id'],
+                PluginFooterLink::getType(),
+                'url',
+                $_SESSION['glpilanguage'],
+                $data['url'],
+            ),
+            'name' => Sanitizer::decodeHtmlSpecialChars(DropdownTranslation::getTranslatedValue(
+                $data['id'],
+                PluginFooterLink::getType(),
+                'name',
+                $_SESSION['glpilanguage'],
+                $data['name'],
+            )),
+        ];
+    }
 
-	echo json_encode($link);
+    echo json_encode($link);
 } elseif (isset($_POST['action']) && $_POST['action'] == 'get_menu') {
-	$interface = 'helpdesk';
-	if (isset($_SESSION['glpiactiveprofile']['interface'])) {
-		$interface = $_SESSION['glpiactiveprofile']['interface'];
-	}
-	$query = [
-		'FROM' => PluginFooterMenu::getTable(),
-		'WHERE' => [
-			'interface' => ['both', $interface],
-		] + getEntitiesRestrictCriteria(PluginFooterMenu::getTable(), '', '', true),
-	];
-	$link = [];
-	foreach ($DB->request($query) as $data) {
-		$link[] = [
-			'url' => DropdownTranslation::getTranslatedValue(
-				$data['id'],
-				PluginFooterMenu::getType(),
-				'url',
-				$_SESSION['glpilanguage'],
-				$data['url']
-			),
-			'name' => Sanitizer::decodeHtmlSpecialChars(DropdownTranslation::getTranslatedValue(
-				$data['id'],
-				PluginFooterMenu::getType(),
-				'name',
-				$_SESSION['glpilanguage'],
-				$data['name']
-			)),
-			'icon' => $data['icon'],
-			'target' => ($data['is_blank']) ? '_blank' : '_self',
-		];
-	}
-	echo json_encode($link);
+    $interface = 'helpdesk';
+    if (isset($_SESSION['glpiactiveprofile']['interface'])) {
+        $interface = $_SESSION['glpiactiveprofile']['interface'];
+    }
+    $query = [
+        'FROM' => PluginFooterMenu::getTable(),
+        'WHERE' => [
+            'interface' => ['both', $interface],
+        ] + getEntitiesRestrictCriteria(PluginFooterMenu::getTable(), '', '', true),
+    ];
+    $link = [];
+    foreach ($DB->request($query) as $data) {
+        $link[] = [
+            'url' => DropdownTranslation::getTranslatedValue(
+                $data['id'],
+                PluginFooterMenu::getType(),
+                'url',
+                $_SESSION['glpilanguage'],
+                $data['url'],
+            ),
+            'name' => Sanitizer::decodeHtmlSpecialChars(DropdownTranslation::getTranslatedValue(
+                $data['id'],
+                PluginFooterMenu::getType(),
+                'name',
+                $_SESSION['glpilanguage'],
+                $data['name'],
+            )),
+            'icon' => $data['icon'],
+            'target' => ($data['is_blank']) ? '_blank' : '_self',
+        ];
+    }
+    echo json_encode($link);
 }
--- hook.php
+++ hook.php
@@ -1,78 +1,89 @@
 <?php
-/*
- -------------------------------------------------------------------------
- Footer plugin for GLPI
- Copyright (C) 2024 by the TICgal Team.
- https://www.tic.gal
- -------------------------------------------------------------------------
- LICENSE
- This file is part of the Footer plugin.
- Footer plugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
- Footer plugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with Footer. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- @package   Footer
- @author    the TICgal team
- @copyright Copyright (c) 2024 TICgal team
- @license   AGPL License 3.0 or (at your option) any later version
-				http://www.gnu.org/licenses/agpl-3.0-standalone.html
- @link      https://www.tic.gal
- @since     2024
- ----------------------------------------------------------------------
-*/
 
-function plugin_footer_install()
+/**
+ * -------------------------------------------------------------------------
+ * Footer plugin for GLPI
+ * Copyright (C) 2025 by the TICGAL Team.
+ * https://www.tic.gal
+ * -------------------------------------------------------------------------
+ * LICENSE
+ * This file is part of the Footer plugin.
+ * Footer plugin is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * Footer plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Footer. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @package   footer
+ * @author    the TICGAL team
+ * @copyright Copyright (c) 2024-2025 TICGAL team
+ * @license   AGPL License 3.0 or (at your option) any later version
+ *            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link      https://www.tic.gal
+ * @since     2024
+ * -------------------------------------------------------------------------
+ */
+
+/**
+ * @return bool
+ */
+function plugin_footer_install(): bool
 {
-	$migration = new Migration(PLUGIN_FOOTER_VERSION);
+    $migration = new Migration(PLUGIN_FOOTER_VERSION);
 
-	foreach (glob(dirname(__FILE__) . '/inc/*') as $filepath) {
-		if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
-			$classname = 'PluginFooter' . ucfirst($matches[1]);
-			include_once($filepath);
-			if (method_exists($classname, 'install')) {
-				$classname::install($migration);
-			}
-		}
-	}
-	$migration->executeMigration();
+    foreach (glob(dirname(__FILE__) . '/inc/*') as $filepath) {
+        if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+            $classname = 'PluginFooter' . ucfirst($matches[1]);
+            include_once($filepath);
+            if (method_exists($classname, 'install')) {
+                $classname::install($migration);
+            }
+        }
+    }
+    $migration->executeMigration();
 
-	return true;
+    return true;
 }
 
-function plugin_footer_uninstall()
+/**
+ * @return bool
+ */
+function plugin_footer_uninstall(): bool
 {
-	$migration = new Migration(PLUGIN_FOOTER_VERSION);
+    $migration = new Migration(PLUGIN_FOOTER_VERSION);
 
-	foreach (glob(dirname(__FILE__) . '/inc/*') as $filepath) {
-		if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
-			$classname = 'PluginFooter' . ucfirst($matches[1]);
-			include_once($filepath);
-			if (method_exists($classname, 'uninstall')) {
-				$classname::uninstall($migration);
-			}
-		}
-	}
-	$migration->executeMigration();
+    foreach (glob(dirname(__FILE__) . '/inc/*') as $filepath) {
+        if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+            $classname = 'PluginFooter' . ucfirst($matches[1]);
+            include_once($filepath);
+            if (method_exists($classname, 'uninstall')) {
+                $classname::uninstall($migration);
+            }
+        }
+    }
+    $migration->executeMigration();
 
-	return true;
+    return true;
 }
 
-function plugin_footer_getDropdown() {
-	$plugin = new Plugin();
+/**
+ * @return array
+ */
+function plugin_footer_getDropdown(): array
+{
+    $plugin = new Plugin();
 
-	if ($plugin->isActivated("footer")) {
-		return [
-			'PluginFooterLink' => PluginFooterLink::getTypeName(),
-			'PluginFooterMenu' => PluginFooterMenu::getTypeName()
-		];
-	}
+    if ($plugin->isActivated("footer")) {
+        return [
+            'PluginFooterLink' => PluginFooterLink::getTypeName(),
+            'PluginFooterMenu' => PluginFooterMenu::getTypeName(),
+        ];
+    }
 
-	return [];
+    return [];
 }
